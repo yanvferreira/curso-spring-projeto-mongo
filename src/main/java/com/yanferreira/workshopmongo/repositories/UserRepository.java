17c222f6b765894fa43a9c8b2e9f4824ec15d056
Yan Ferreira <yanfvc@gmail.com>
2026-01-19 00:28:41 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/yanferreira/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.yanferreira.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yanferreira.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/yanferreira/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.yanferreira.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yanferreira.workshopmongo.domain.User;
+import com.yanferreira.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
