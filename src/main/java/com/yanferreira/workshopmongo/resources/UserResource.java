ffa1481852eed68075a95310dfd66fd6408a9d27
Yan Ferreira <yanfvc@gmail.com>
2026-01-19 23:07:29 -0300

    Atualização de usuário com PUT

--- src/main/java/com/yanferreira/workshopmongo/resources/UserResource.java
+++ src/main/java/com/yanferreira/workshopmongo/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.yanferreira.workshopmongo.domain.User;
 import com.yanferreira.workshopmongo.dto.UserDto;
 import com.yanferreira.workshopmongo.services.UserService;
+
 
 
 
@@ -51,6 +53,14 @@ public class UserResource {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> putMethodName(@PathVariable String id, @RequestBody UserDto userDto) {
+        User objUser = service.fromDto(userDto);
+        objUser.setId(id);
+        objUser = service.update(objUser);
         return ResponseEntity.noContent().build();
     }
     
