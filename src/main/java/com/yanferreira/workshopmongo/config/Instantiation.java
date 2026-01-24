631c044478a9e398c2ad21878ad1e47f86bd4e1b
Yan Ferreira <yanfvc@gmail.com>
2026-01-24 12:50:03 -0300

    Referenciando os posts do usuário

--- src/main/java/com/yanferreira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/yanferreira/workshopmongo/config/Instantiation.java
@@ -41,6 +41,9 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, LocalDate.parse("23/03/2018", dateFormatter), "Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 
 }
