a16b89d71aafe0612a3d26065ae1be271b4a15f3
Yan Ferreira <yanfvc@gmail.com>
2026-01-25 17:01:26 -0300

    Consulta simples com query methods

--- src/main/java/com/yanferreira/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/yanferreira/workshopmongo/repositories/PostRepository.java
@@ -1,11 +1,15 @@
 package com.yanferreira.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.yanferreira.workshopmongo.domain.Post;
 
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+    
+    List<Post> findByTitleContainingIgnoreCase(String title);
 }
--- src/main/java/com/yanferreira/workshopmongo/resources/PostResource.java
+++ src/main/java/com/yanferreira/workshopmongo/resources/PostResource.java
@@ -1,14 +1,19 @@
 package com.yanferreira.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yanferreira.workshopmongo.domain.Post;
+import com.yanferreira.workshopmongo.resources.util.URL;
 import com.yanferreira.workshopmongo.services.PostService;
+
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,4 +27,12 @@ public class PostResource {
         Post post = service.findById(id);
         return ResponseEntity.ok(post);
     }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> posts = service.findByTitle(text);
+        return ResponseEntity.ok(posts);
+    }
+    
 }
